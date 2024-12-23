0d6d8d1efb410ca3e32b582af86c9e2a8875ea61
Sindhu-S-Gowdar <sindhusgowdar77@gmail.com>
2024-12-23 22:06:34 +0530

    Add files via upload
    files uploaded

---
+++ campus.js
@@ -0,0 +1,8 @@
+function scrollToSection(sectionId) {
+    document.getElementById(sectionId).scrollIntoView({ behavior: "smooth" });
+  }
+  
+  function showEventDetails(eventName) {
+    alert(`More details about ${eventName} coming soon!`);
+  }
+  
